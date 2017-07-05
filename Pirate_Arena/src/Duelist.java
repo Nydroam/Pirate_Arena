36640570671ed70dd2d90cacf627e2fc782a2469
tonyli7 <xxtonyxxlixx@gmail.com>
2017-07-04 22:39:19 -0400

    Update Duelist.java

--- Pirate_Arena/src/Duelist.java
+++ Pirate_Arena/src/Duelist.java
@@ -19,13 +19,17 @@ public class Duelist extends Pirate{
   public void special(Pirate other) {
     System.out.println(name + " ate an Orenji and felt empowered. He leaped forward and struck with all his might.");
     Random rand = new Random();
-    other.takeDmg(STR + rand.nextInt(16) + 5);
+    int extra = rand.nextInt(16) + 5;
+    other.takeDmg(STR + extra);
+    System.out.println(other.getName() + " took " + (STR + extra) + " damage.");
     lowerStat(this, "ACC", 10);
+    displayHP(other);
     System.out.println("He now feels a little tired.");
   }
 
   public void taunt(Pirate other) {
     System.out.println("Ye can't face meh. Ye just a lad");
     other.lowerStat(other, "DEX", 5);
+    System.out.println(other.getName() + "'s DEX has been lowered.");
   }
 }
